800400ed54a3d0a1bfd022638f650f6b23d6919f
VincentLabor <vlabor95@gmail.com>
2021-12-08 18:06:42 -0800

    Users can no longer see the adminbar

--- functions.php
+++ functions.php
@@ -14,34 +14,34 @@ $understrap_inc_dir = get_template_directory() . '/inc';
 
 // Array of files to include.
 $understrap_includes = array(
-	'/theme-settings.php',                  // Initialize theme default settings.
-	'/setup.php',                           // Theme setup and custom theme supports.
-	'/widgets.php',                         // Register widget area.
-	'/enqueue.php',                         // Enqueue scripts and styles.
-	'/template-tags.php',                   // Custom template tags for this theme.
-	'/pagination.php',                      // Custom pagination for this theme.
-	'/hooks.php',                           // Custom hooks.
-	'/extras.php',                          // Custom functions that act independently of the theme templates.
-	'/customizer.php',                      // Customizer additions.
-	'/custom-comments.php',                 // Custom Comments file.
-	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567.
-	'/editor.php',                          // Load Editor functions.
-	'/deprecated.php',                      // Load deprecated functions.
+  '/theme-settings.php',                  // Initialize theme default settings.
+  '/setup.php',                           // Theme setup and custom theme supports.
+  '/widgets.php',                         // Register widget area.
+  '/enqueue.php',                         // Enqueue scripts and styles.
+  '/template-tags.php',                   // Custom template tags for this theme.
+  '/pagination.php',                      // Custom pagination for this theme.
+  '/hooks.php',                           // Custom hooks.
+  '/extras.php',                          // Custom functions that act independently of the theme templates.
+  '/customizer.php',                      // Customizer additions.
+  '/custom-comments.php',                 // Custom Comments file.
+  '/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567.
+  '/editor.php',                          // Load Editor functions.
+  '/deprecated.php',                      // Load deprecated functions.
 );
 
 // Load WooCommerce functions if WooCommerce is activated.
 if (class_exists('WooCommerce')) {
-	$understrap_includes[] = '/woocommerce.php';
+  $understrap_includes[] = '/woocommerce.php';
 }
 
 // Load Jetpack compatibility file if Jetpack is activiated.
 if (class_exists('Jetpack')) {
-	$understrap_includes[] = '/jetpack.php';
+  $understrap_includes[] = '/jetpack.php';
 }
 
 // Include files.
 foreach ($understrap_includes as $file) {
-	require_once $understrap_inc_dir . $file;
+  require_once $understrap_inc_dir . $file;
 }
 
 
@@ -67,9 +67,8 @@ function pageBanner($args = NULL)
 
 ?>
 
-<!-- This is the page event banners and the text works but the pictures do no atm.  -->
-<!-- Initially any edits did not appear because of the error that was within the sass/gulp -->
-<div class="page-banner bot3emMargin">
+  <!-- This is the default header for events -->
+  <div class="page-banner bot3emMargin">
     <div class="page-banner__bg-image flexImage">
       <img src="<?php echo $args['photo']; ?>" alt="" class="bannerImg">
     </div>
@@ -81,4 +80,26 @@ function pageBanner($args = NULL)
     </div>
   </div>
 <?php
- }
+}
+
+//Redirecting basic users to homepage instead of dashboard.
+add_action('admin_init', 'redirectUserstoFrontEnd');
+function redirectUserstoFrontEnd()
+{
+  $ourCurrentUser = wp_get_current_user();
+  //This is specifically for the lowest tier user.
+  if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit;
+  }
+}
+//Removing the wp_admin bar thing
+add_action('wp_loaded', 'noSubsAdminBar');
+function noSubsAdminBar()
+{
+  $ourCurrentUser = wp_get_current_user();
+  //This is specifically for the lowest tier user.
+  if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
