9582233e7f799f9b98381ba712faf7d0b586b671
VincentLabor <vlabor95@gmail.com>
2022-03-10 16:12:53 -0800

    Fixed AOW layout and event archive

--- single-event-AOW.php
+++ single-event-AOW.php
@@ -29,7 +29,26 @@ get_header();
 
 
 <?php
-// the_content();
-get_footer();
+?>
+<div class="aowBg centerflex whiteText centerflexColumnMobileAOW">
+    <img src="<?php echo get_template_directory_uri();
+                ?>/img/aowImage_357w_500h.png" alt="" class=" ">
 
+    <div class="eventTextAOW aowText">
+        <u>
+            <h2>Art of Worship 2022</h2>
+        </u>
+
+        <p class=""><?php the_content(); ?></p>
+
+        <!-- <p>Many people from our JesusWalk community will be showcasing the artistic talents that God has blessed them with.
+            When it comes to worshiping God, there are endless possibilities! Join us on YouTube Live on April 30 - May 2 at 7:00pm.</p> -->
+    </div>
+
+
+</div>
+
+<?php
+
+get_footer();
 ?>
